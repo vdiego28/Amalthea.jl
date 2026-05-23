f692e06b1c74c3b63fc0b522f179c0371ecc2d13
vdiego28 <vdiego28@yahoo.es>
2026-05-23 18:24:12 -0400

    Remove target directory from tracked files

--- luna-rust/target/release/build/num-traits-def6319ac3ccfa39/build_script_build-def6319ac3ccfa39.d
+++
@@ -1,5 +0,0 @@
-/home/diego/Documents/fernando_luz/Luna-Rust.jl/luna-rust/target/release/build/num-traits-def6319ac3ccfa39/build_script_build-def6319ac3ccfa39.d: /home/diego/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/diego/Documents/fernando_luz/Luna-Rust.jl/luna-rust/target/release/build/num-traits-def6319ac3ccfa39/build_script_build-def6319ac3ccfa39: /home/diego/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
-
-/home/diego/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs:
