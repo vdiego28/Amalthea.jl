9321a360691754be9d19a64ad437cea03eb16614
vdiego28 <vdiego28@yahoo.es>
2026-06-27 09:31:06 -0400

    fix(ci): preserve environment variables when passing CARGO_BUILD_RUSTFLAGS

--- deps/build.jl
+++ deps/build.jl
@@ -11,8 +11,8 @@ Pkg.build("PyCall")
 rust_dir = joinpath(@__DIR__, "..", "luna-rust")
 if isdir(rust_dir)
     try
-        run(Cmd(`cargo build --release`, dir=rust_dir,
-                env=Dict("CARGO_BUILD_RUSTFLAGS" => get(ENV, "CARGO_BUILD_RUSTFLAGS", ""))))
+        run(addenv(Cmd(`cargo build --release`, dir=rust_dir),
+                   "CARGO_BUILD_RUSTFLAGS" => get(ENV, "CARGO_BUILD_RUSTFLAGS", "")))
         @info "Successfully compiled Rust library luna-rust."
     catch e
         @error "Failed to compile Rust library luna-rust: make sure Rust/Cargo (version >= 1.85) is installed on your system." e
